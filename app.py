37d622bd1a5f0424a6b98556b2f4fc5f17f1ec1d
unknownman_ <jose.wibowo@binus.ac.id>
2025-09-23 23:04:39 +0700

    Switch from FastAPI to Flask for web app

--- app.py
+++ app.py
@@ -1,19 +1,16 @@
-from fastapi import FastAPI, UploadFile, File
-from fastapi.responses import FileResponse
-import uuid, os
+from flask import Flask, render_template, request
 from generate import generate_rule
 
-app = FastAPI()
+app = Flask(__name__)
 
-@app.post('/generate')
-async def generate(file: UploadFile = File(...)):
-content = (await file.read()).decode()
-xml = generate_rule(content)
-fname = f"/tmp/{uuid.uuid4()}.xml"
-with open(fname, 'w') as f:
-f.write(xml)
-return FileResponse(fname, media_type='application/xml', filename='wazuh_rule.xml')
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    generated_rule = ""
+    if request.method == 'POST':
+        log_text = request.form.get('log_text')
+        if log_text:
+            generated_rule = generate_rule(log_text)
+    return render_template('index.html', generated_rule=generated_rule)
 
-@app.get('/health')
-def health():
-return {'status':'ok'}
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080, debug=True)
